bfbbb8c3d0e4b0665ed25d10d11de7d1eab0597b
dariusz.izak@gmail.com <dariusz.izak@gmail.com>
2017-12-25 17:47:40 +0100

    UPD:Copied pattern from index to other views. Should be replaced with single view and url pattern

--- rolety_perfect/views.py
+++ rolety_perfect/views.py
@@ -14,24 +14,54 @@ def index(request):
 
 
 def verticale(request):
-    return render(request, "rolety_perfect/verticale.html.jj2")
+    page = Page.objects.get(url="verticale.html")
+    content = Page.objects.get(url="verticale.html").post.content
+    return render(request,
+                  "rolety_perfect/verticale.html.jj2",
+                  {"page": page,
+                   "content": content})
 
 
 def zaluzje(request):
-    return render(request, "rolety_perfect/zaluzje.html.jj2")
+    page = Page.objects.get(url="zaluzje.html")
+    content = Page.objects.get(url="zaluzje.html").post.content
+    return render(request,
+                  "rolety_perfect/zaluzje.html.jj2",
+                  {"page": page,
+                   "content": content})
 
 
 def rolety_materialowe(request):
-    return render(request, "rolety_perfect/rolety-materialowe.html.jj2")
+    page = Page.objects.get(url="rolety-materialowe.html")
+    content = Page.objects.get(url="rolety-materialowe.html").post.content
+    return render(request,
+                  "rolety_perfect/rolety-materialowe.html.jj2",
+                  {"page": page,
+                   "content": content})
 
 
 def rolety_zewnetrzne(request):
-    return render(request, "rolety_perfect/rolety-zewnetrzne.html.jj2")
+    page = Page.objects.get(url="rolety-zewnetrzne.html")
+    content = Page.objects.get(url="rolety-zewnetrzne.html").post.content
+    return render(request,
+                  "rolety_perfect/rolety-zewnetrzne.html.jj2",
+                  {"page": page,
+                   "content": content})
 
 
 def moskitiery(request):
-    return render(request, "rolety_perfect/moskitiery.html.jj2")
+    page = Page.objects.get(url="moskitiery.html")
+    content = Page.objects.get(url="moskitiery.html").post.content
+    return render(request,
+                  "rolety_perfect/moskitiery.html.jj2",
+                  {"page": page,
+                   "content": content})
 
 
 def kontakt(request):
-    return render(request, "rolety_perfect/kontakt.html.jj2")
+    page = Page.objects.get(url="kontakt.html")
+    content = Page.objects.get(url="kontakt.html").post.content
+    return render(request,
+                  "rolety_perfect/kontakt.html.jj2",
+                  {"page": page,
+                   "content": content})

d41657d7cd19a72c398c6e11a9bd639c689160a3
dariusz.izak@gmail.com <dariusz.izak@gmail.com>
2017-12-26 13:42:35 +0100

    UPD:Single view for any page

--- rolety_perfect/urls.py
+++ rolety_perfect/urls.py
@@ -3,11 +3,11 @@ from rolety_perfect import views
 
 app_name = "rolety_perfect"
 
-urlpatterns = [url(r"^$", views.index, name="index"),
-               url(r"^index.html/$", views.index, name="index"),
-               url(r"^verticale.html/$", views.verticale, name="verticale"),
-               url(r"^zaluzje.html/$", views.zaluzje, name="zaluzje"),
-               url(r"^rolety-materialowe.html/$", views.rolety_materialowe, name="rolety_materialowe"),
-               url(r"^rolety-zewnetrzne.html/$", views.rolety_zewnetrzne, name="rolety_zewnetrzne"),
-               url(r"^moskitiery.html/$", views.moskitiery, name="moskitiery"),
-               url(r"^kontakt.html/$", views.kontakt, name="kontakt")]
+urlpatterns = [url(r"^$", views.page, name="index"),
+               url(r"^index.html/$", views.page, name="index"),
+               url(r"^verticale.html/$", views.page, name="verticale"),
+               url(r"^zaluzje.html/$", views.page, name="zaluzje"),
+               url(r"^rolety-materialowe.html/$", views.page, name="rolety_materialowe"),
+               url(r"^rolety-zewnetrzne.html/$", views.page, name="rolety_zewnetrzne"),
+               url(r"^moskitiery.html/$", views.page, name="moskitiery"),
+               url(r"^kontakt.html/$", views.page, name="kontakt")]
--- rolety_perfect/views.py
+++ rolety_perfect/views.py
@@ -2,66 +2,17 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from rolety_perfect.models import *
+from django.http import HttpResponse
 
 
-def index(request):
-    page = Page.objects.get(url="index.html")
-    content = Page.objects.get(url="index.html").post.content
+def page(request):
+    if request.path == "/" or request.path == "":
+        model_url = "index.html"
+    else:
+        model_url = request.path.strip("/")
+    page = Page.objects.get(url=model_url)
+    content = Page.objects.get(url=model_url).post.content
     return render(request,
-                  "rolety_perfect/index.html.jj2",
-                  {"page": page,
-                   "content": content})
-
-
-def verticale(request):
-    page = Page.objects.get(url="verticale.html")
-    content = Page.objects.get(url="verticale.html").post.content
-    return render(request,
-                  "rolety_perfect/verticale.html.jj2",
-                  {"page": page,
-                   "content": content})
-
-
-def zaluzje(request):
-    page = Page.objects.get(url="zaluzje.html")
-    content = Page.objects.get(url="zaluzje.html").post.content
-    return render(request,
-                  "rolety_perfect/zaluzje.html.jj2",
-                  {"page": page,
-                   "content": content})
-
-
-def rolety_materialowe(request):
-    page = Page.objects.get(url="rolety-materialowe.html")
-    content = Page.objects.get(url="rolety-materialowe.html").post.content
-    return render(request,
-                  "rolety_perfect/rolety-materialowe.html.jj2",
-                  {"page": page,
-                   "content": content})
-
-
-def rolety_zewnetrzne(request):
-    page = Page.objects.get(url="rolety-zewnetrzne.html")
-    content = Page.objects.get(url="rolety-zewnetrzne.html").post.content
-    return render(request,
-                  "rolety_perfect/rolety-zewnetrzne.html.jj2",
-                  {"page": page,
-                   "content": content})
-
-
-def moskitiery(request):
-    page = Page.objects.get(url="moskitiery.html")
-    content = Page.objects.get(url="moskitiery.html").post.content
-    return render(request,
-                  "rolety_perfect/moskitiery.html.jj2",
-                  {"page": page,
-                   "content": content})
-
-
-def kontakt(request):
-    page = Page.objects.get(url="kontakt.html")
-    content = Page.objects.get(url="kontakt.html").post.content
-    return render(request,
-                  "rolety_perfect/kontakt.html.jj2",
+                  "rolety_perfect/{}.jj2".format(model_url),
                   {"page": page,
                    "content": content})
